0b46708c5f3de787d580a96731c25b2b72e1e017
Vinithfarooz <vinithfarooz25@gmail.com>
2021-12-31 20:54:55 +0530

    Username Functionality

---
+++ src/org/in/Oppo.java
@@ -0,0 +1,10 @@
+package org.in;
+
+public class Oppo {
+	private static void username() {
+System.out.println("Username");
+	}
+public static void main(String[] args) {
+	username();
+}
+}
---
+++ src/org/in/Realme.java
@@ -0,0 +1,10 @@
+package org.in;
+
+public class Realme {
+private static void Password() {
+System.out.println("Password");
+}
+public static void main(String[] args) {
+	Password();
+}
+}
